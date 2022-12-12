f073f7dea65cbaf2db4caf92e364d1233d2ac1f6
Volodymyr Onyshchenko <volodymyr.onyshchenko.km13@gmail.com>
2022-12-12 16:22:36 +0200

    Added IntMatrixImmutable class

---
+++ src/main/java/oop/labs/lab2/IntMatrixImmutable.java
@@ -0,0 +1,58 @@
+package oop.labs.lab2;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class IntMatrixImmutable extends IntMatrix
+{
+    private static List<List<Integer>> makeImmutable(List<List<Integer>> matrix)
+    {
+        matrix.replaceAll(Collections::unmodifiableList);
+        return Collections.unmodifiableList(matrix);
+    }
+
+
+    public IntMatrixImmutable()
+    {
+        super(makeImmutable(buildMatrix()));
+    }
+
+    public IntMatrixImmutable(int rows, int cols, Integer init)
+    {
+        super(makeImmutable(buildMatrix(rows, cols, init)));
+    }
+
+    public IntMatrixImmutable(int[][] matrix)
+    {
+        super(makeImmutable(buildMatrix(matrix)));
+    }
+
+    public <TRow extends Iterable<Integer>> IntMatrixImmutable(Iterable<TRow> source)
+    {
+        super(makeImmutable(buildMatrix(source)));
+    }
+
+    public IntMatrixImmutable(MatrixOfIntegers other)
+    {
+        super(makeImmutable(buildMatrix(other)));
+    }
+
+
+    @Override
+    public MatrixOfIntegers dot(MatrixOfIntegers multiplier)
+    {
+        return null;
+    }
+
+
+    @Override public Matrix<Integer> set(int row, int col, Integer value) { throw new UnsupportedOperationException(); }
+
+    @Override public Matrix<Integer> fillRow(int index, Iterable<Integer> source) { throw new UnsupportedOperationException(); }
+    @Override public Matrix<Integer> fillCol(int index, Iterable<Integer> source) { throw new UnsupportedOperationException(); }
+
+    @Override public Matrix<Integer> fillRow(int index, int[] source) { throw new UnsupportedOperationException(); }
+    @Override public Matrix<Integer> fillCol(int index, int[] source) { throw new UnsupportedOperationException(); }
+
+    @Override public Matrix<Integer> fill(int[][] source) { throw new UnsupportedOperationException(); }
+    @Override public <TRow extends Iterable<Integer>> Matrix<Integer> fill(Iterable<TRow> source) { throw new UnsupportedOperationException(); }
+}
