9840608fc3785566118244d43bc8059f7d51cda7
Volodymyr Onyshchenko <volodymyr.onyshchenko.km13@gmail.com>
2022-12-12 16:21:30 +0200

    Added Matrix generic interface

---
+++ src/main/java/oop/labs/lab2/Matrix.java
@@ -0,0 +1,25 @@
+package oop.labs.lab2;
+
+public interface Matrix<TElement>
+{
+    int[] size();
+
+    int rows();
+    int cols();
+
+    Integer get(int row, int col);
+
+    Iterable<Integer> row(int index);
+    Iterable<Integer> col(int index);
+
+    Matrix<TElement> set(int row, int col, TElement value);
+
+    Matrix<TElement> fillRow(int index, Iterable<TElement> source);
+    Matrix<TElement> fillCol(int index, Iterable<TElement> source);
+
+    Matrix<TElement> fillRow(int index, int[] source);
+    Matrix<TElement> fillCol(int index, int[] source);
+
+    Matrix<TElement> fill(int[][] source);
+    <TRow extends Iterable<TElement>> Matrix<TElement> fill(Iterable<TRow> source);
+}
